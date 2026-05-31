067de09a52d874ffa64ff1363116976120f5259d
Eitan-Oz <bachareitanoz@gmail.com>
2026-05-31 09:54:14 +0300

    change BaseDal

--- DAL/BaseDal.cs
+++ DAL/BaseDal.cs
@@ -1,40 +1,39 @@
-﻿using System;
+﻿﻿using System;
 using System.Data;
-using System.Data.SqlClient;
+using System.Data.OleDb;
 using System.IO;
-
 
 namespace DAL
 {
     public class BaseDal
     {
         protected string ConnectionString;
-        protected SqlConnection conn;
+        protected OleDbConnection conn;
+
         public BaseDal()
         {
-            // נתיב מוחלט -לא מומלץ
-            // ConnectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=F:\\MyProject\\LayersExample2\\DAL\\Database1.mdf;Integrated Security=True";
+            // Path to Access DB file
+            string DBfileName = @"data\mashovdb.accdb";
 
-            // נתיב יחסי למיקום קובץ הדטה בייס  
-            string DBfileName = "data\\mashovdb.accdb";
-            // צריך להוריד  2 תיקיות מהסוף
-            // (bin → Debug ) שם ממוקם הדטה בייס ברירת מחדל
+            // Move up from bin\Debug (or bin\Release) to project root
             string projectDir = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\"));
-            ConnectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=" +
-                projectDir + DBfileName + ";Integrated Security=True";
+            string dbFullPath = Path.Combine(projectDir, DBfileName);
 
-            // יצירת האובייקט שמייצג את החיבור לדטה בייס
-            conn = new SqlConnection(ConnectionString);
+            // Access connection string (.accdb)
+            ConnectionString = $@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source={dbFullPath};Persist Security Info=False;";
+            conn = new OleDbConnection(ConnectionString);
         }
 
         public int ExecuteInsertQuery(string sql)
         {
             return ExecuteQuery(sql);
         }
+
         public int ExecuteUpdateQuery(string sql)
         {
             return ExecuteQuery(sql);
         }
+
         public int ExecuteDeleteQuery(string sql)
         {
             return ExecuteQuery(sql);
@@ -42,23 +41,24 @@ namespace DAL
 
         /// <summary>
         /// מפעיל שאילתא שמחזירה טבלה
-        /// למשל
+        /// למשל:
         /// select * from Students where Gender='male'
+        /// </summary>
         public DataTable ExecuteSelectAllQuery(string sql)
         {
             DataTable table = new DataTable();
-            SqlCommand cmd = new SqlCommand(sql, conn);
+            OleDbCommand cmd = new OleDbCommand(sql, conn);
 
             try
             {
                 conn.Open();
-                SqlDataReader reader = cmd.ExecuteReader();
+                OleDbDataReader reader = cmd.ExecuteReader();
                 table.Load(reader);
             }
             catch (Exception ex)
             {
                 HandleError(ex, sql);
-                throw; // חשוב! לא לבלוע שגיאה
+                throw;
             }
             finally
             {
@@ -71,8 +71,6 @@ namespace DAL
         /// <summary>
         /// מפעיל שאילתה שמחזירה מחרוזת
         /// </summary>
-        /// <param name="sql">The sql qurey to execute</param>        
-        /// <returns>the scalar execution converted to string</returns>
         public string ExecuteSelectStringQuery(string sql)
         {
             object obj = ExecuteSelectOneData(sql);
@@ -82,8 +80,6 @@ namespace DAL
         /// <summary>
         /// מפעיל שאילתה שמחזירה ערך בוליאני
         /// </summary>
-        /// <param name="sql">The sql qurey to execute</param>        
-        /// <returns>the scalar execution converted to boolean</returns>
         public bool ExecuteSelectBoolQuery(string sql)
         {
             object obj = ExecuteSelectOneData(sql);
@@ -93,8 +89,6 @@ namespace DAL
         /// <summary>
         /// מפעיל שאילתה שמחזירה ערך שלם
         /// </summary>
-        /// <param name="sql">The sql qurey to execute</param>        
-        /// <returns>the scalar execution converted to int</returns>
         public int ExecuteSelectIntQuery(string sql)
         {
             object obj = ExecuteSelectOneData(sql);
@@ -104,11 +98,9 @@ namespace DAL
         /// <summary>
         /// מפעיל שאילתה שמחזירה ערך יחיד
         /// </summary>
-        /// <param name="sql"></param>
-        /// <returns></returns>
         private object ExecuteSelectOneData(string sql)
         {
-            SqlCommand cmd = new SqlCommand(sql, conn);
+            OleDbCommand cmd = new OleDbCommand(sql, conn);
 
             try
             {
@@ -128,9 +120,10 @@ namespace DAL
 
         /// <summary>
         /// מפעיל שאילתא שלא מחזירה ערך (INSERT, UPDATE, DELETE)
+        /// </summary>
         private int ExecuteQuery(string sql)
         {
-            SqlCommand cmd = new SqlCommand(sql, conn);
+            OleDbCommand cmd = new OleDbCommand(sql, conn);
 
             try
             {
@@ -150,17 +143,9 @@ namespace DAL
 
         public virtual void HandleError(Exception ex, string sql)
         {
-            // כאן אפשר:
-            // לכתוב לקובץ
-            // לשמור ל-DB
-            // לשלוח לוג
-
-            Console.WriteLine("SQL Error:");
+            Console.WriteLine("DB Error:");
             Console.WriteLine(sql);
             Console.WriteLine(ex.Message);
         }
     }
 }
-
-
-
